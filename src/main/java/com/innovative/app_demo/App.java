cb5d4c36bcbdfcd208086741039133a7200533e0
YagneshTrainer <yagneshyagnesh007@gmail.com>
2023-06-27 17:52:03 +0530

    added inital commit

---
+++ src/main/java/com/innovative/app_demo/App.java
@@ -0,0 +1,13 @@
+package com.innovative.app_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
